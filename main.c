dd63c3ab987c91e002bcfa312ef221163f4f95a0
gmayweat <gmayweat@student.21-school.ru>
2020-12-16 22:42:01 +0300

    woof

--- main.c
+++ main.c
@@ -4,15 +4,15 @@
 
 int main()
 {
-	//int fd = open("meow", O_RDONLY);
-	int fd = open("warandpeace", O_RDONLY);
+	int fd = open("meow", O_RDONLY);
+	//int fd = open("warandpeace", O_RDONLY);
 	char *line;
 	int i;
 	int j = 0;
 
-	while ((i = get_next_line(fd, &line)) != 0)
+	while ((i = get_next_line(0, &line)) != 0)
 	{
-		printf("i = %d %s\n", i, line);
+		printf("j = %d %s\n", j, line);
 		free(line);
 		++j;
 		if(j == 25059)
